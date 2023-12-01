3b6e8081504d1a03c2051df6a28db8696091f200
ldeng <denglin12315@163.com>
2023-12-01 12:40:02 +0800

    optimize general.lua for vim.opt
    Signed-off-by: ldeng <denglin12315@163.com>

--- lua/general.lua
+++ lua/general.lua
@@ -1,14 +1,14 @@
 -- 使用相对行号
-vim.o.number = true
-vim.o.relativenumber = true
+vim.opt.number = true
+vim.opt.relativenumber = true
 
 -- 启用语法高亮
 vim.opt.syntax = 'on'
 
 -- 高亮所在行
-vim.wo.cursorline = true
+vim.opt.cursorline = true
 -- 右侧参考线，超过表示代码太长了，考虑换行
-vim.wo.colorcolumn = "100"
+vim.opt.colorcolumn = "100"
 
 -- 启用文件类型检测和插件
 vim.opt.filetype = 'on'
@@ -18,14 +18,14 @@ vim.cmd('colorscheme monokai')
 
 -- 启用显示可见字符
 vim.opt.list = true
-vim.opt.listchars:append({ tab = '->', trail = '.' })
+vim.opt.listchars:append({tab = '▸ ', trail = '·'})
 
 -- 启用自动缩进
 vim.opt.autoindent = true
 
 -- jkhl 移动时光标周围保留8行
-vim.o.scrolloff = 10
-vim.o.sidescrolloff = 8
+vim.opt.scrolloff = 10
+vim.opt.sidescrolloff = 8
 
 -- 针对 quickfix 窗口设置高度
 vim.cmd('autocmd FileType qf setlocal winheight=14')
